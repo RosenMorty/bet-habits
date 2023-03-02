ea424f8f6e79fbbcc57034efb071c3e42b1eef3f
Владислав <rolit0852@gmail.com>
2023-03-02 16:34:49 +0300

    habbits con

---
+++ scripts/aapp.js
@@ -0,0 +1,14 @@
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+
+function loadData() {
+    const habbitsString = localStorage.getItem('HABBIT_KEY');
+    const habbitArray = JSON.parse(habbitsString);
+    if (Array.isArray(habbitArray)) {
+        habbits = habbitArray;
+    }
+}
+
+function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
